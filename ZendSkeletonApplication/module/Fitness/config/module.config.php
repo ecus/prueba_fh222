074ddf973b00149a5353c222bf24f24a6a9869d9
Erik Urrutia Santamaria <e_urrutia@outlook.com>
2013-06-06 22:55:51 -0500

    sync

--- ZendSkeletonApplication/module/Fitness/config/module.config.php
+++ ZendSkeletonApplication/module/Fitness/config/module.config.php
@@ -1,49 +1,100 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
 return array(
-    'controllers'=>array(
-        'invokables'=>array(
-            'Fitness\Controller\Index'=>'Fitness\Controller\IndexController'
-         ),
-     ),
-     
-     'router'=>array(
-        'routes'=>array(
-            'cesar'=>array(
-                 'type'=>'Segment',
-                    'options'=>array(
-                        //route'    => '/[:controller[/:action][/:id]]'
-                        //'route'    => '/[:controller[/:action]]',
-                        'route' => '/fitness[/[:action]]',
-                        'constraints' => array(
-                                'action'  =>  '[a-zA-Z][a-zA-Z0-9_-]*',
-                        ),
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Fitness\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+            'fitness' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/fitness',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Fitness\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action][/:id]]',
+                            //'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*'
 
-                        'defaults'  =>  array(
-                                'controller' => 'Fitness\Controller\Index',
-                                'action'     => 'index'
-
+                            ),
+                            'defaults' => array(
+                            ),
                         ),
                     ),
-           ),
-       ),
+                ),
+            ),
+        ),
     ),
-
-   //Cargamos el view manager
-   'view_manager' => array(
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
+        ),
+    ),
+    'translator' => array(
+        'locale' => 'es_ES',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Fitness\Controller\Index'=>'Fitness\Controller\IndexController',
+            'Fitness\Controller\Formulario'=>'Fitness\Controller\FormularioController',
+            'Fitness\Controller\Registros'=>'Fitness\Controller\RegistrosController'
+        ),
+    ),
+    'view_manager' => array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'fitness/index/index' => __DIR__ . '/../view/fitness/index/index.phtml',
+            //'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/prueba'           => __DIR__ . '/../view/layout/prueba.phtml',
+            'fitness/index/index'     => __DIR__ . '/../view/fitness/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
-          'fitness' =>  __DIR__ . '/../view',
+           'fitness' => __DIR__ . '/../view',
         ),
     ),
- );
+);
--- ZendSkeletonApplication/module/Fitness/src/Fitness/Controller/IndexController.php
+++ ZendSkeletonApplication/module/Fitness/src/Fitness/Controller/IndexController.php
@@ -3,11 +3,28 @@ namespace Fitness\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Sql\Sql;
+
+use Fitness\Form\Regsucursal;
+use Fitness\Model\Entity\Sucursal;
+
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
-    {
-        return new ViewModel();
-    }
+	public function indexAction()
+	{
+		//return new ViewModel();
+		$pag	=	$this->getRequest()->getBaseUrl();
+		$form 	=	new Regsucursal('form');
+		$var	=	array(
+				"titulo"	=>	"holaaaa carajo este es mi formulario en ZF2",
+    			"form"		=>	$form,
+				"url"		=>	$pag
+			);
+		$view = new ViewModel($var);
+		//$this->layout('layout/prueba');
+		return $view;
+	}
 }
