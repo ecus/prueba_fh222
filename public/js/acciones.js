14a45728d94d1ad083b804a4de67f2e028d9a528
Erik Urrutia Santamaria <e_urrutia@outlook.com>
2013-10-11 19:27:18 -0500

    333

--- public/js/acciones.js
+++ public/js/acciones.js
@@ -1,6 +1,30 @@
-$(".form_datetime").datetimepicker({
-format: "dd MM yyyy - hh:ii",
-autoclose: true,
-todayBtn: true,
-pickerPosition: "bottom-left"
+jQuery(function($) {
+    var paginas = $(this)['context'].URL.toString().split("/");
+    var strPag  = paginas[paginas.length-1];
+    var controller=['fitness','registros','atencion'];
+    $('.numerico').validarCampo('0123456789');
+    $('.moneda').validarCampo('.0123456789');
+    $('.soloTexto').validarCampo(' abcdefghijklmnñopqrstuvwxyzáéíóú');
+    $('.soloTextoNombre').validarCampo(' abcdefghijklmnñopqrstuvwxyzáéíóú-123456789');
+    $('.email').validarCampo('abcdefghijklmnopqrstuvwxyz0123456789_-.@');
+    $('.direccion').validarCampo(' abcdefghijklmnopqrstuvwxyz0123456789_-#°.');
+    if(jQuery.inArray(strPag, controller)>0){
+        strPag="index";
+    pace.start();
+
+        cargaJS(strPag);
+    }else{
+        cargaJS(strPag);
+    }
 });
+
+function cargaJS(accion) {
+    var ele = document.getElementById(accion);
+    if (ele == undefined) {
+        var tagjs = document.createElement("script");
+        tagjs.setAttribute("type", "text/javascript");
+        tagjs.setAttribute("id", accion);
+        tagjs.setAttribute("src", "../../js/"+accion+".js");
+        document.getElementsByTagName("head")[0].appendChild(tagjs);
+    }
+}
